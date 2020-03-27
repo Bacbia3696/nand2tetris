803ad15b17d1d4505a45bc1ef6c20e8a2386fb4f
DatNguyen <bacbia3696@gmail.com>
2020-03-27 18:33:49 +0700

    Finished project 4 - So hard!

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,65 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+    @KBD
+    D=M;
+    @PRESSED
+    D; JNE
+    // CLEAR SCREEN
+
+    // i=0
+    @i
+    M=0
+(CLEAR)
+    // i == 8192 goto LOOP
+    @8192
+    D=A
+    @i
+    D=D-M;
+    @LOOP
+    D; JEQ
+
+    // clear
+    @i
+    D=M
+    @SCREEN
+    A=D+A
+    M=0
+
+    // i = i + 1
+    @i
+    M=M+1
+
+    @CLEAR
+    0; JMP
+
+(PRESSED)
+    // DRAW SCREEN
+
+    // i=0
+    @i
+    M=0
+(DRAW)
+    // i == 8192 goto LOOP
+    @8192
+    D=A
+    @i
+    D=D-M
+    @LOOP
+    D; JEQ
+
+    // draw
+    @i
+    D=M
+    @SCREEN
+    A=D+A
+    M=-1
+
+    // i = i + 1
+    @i
+    M=M+1
+
+    @DRAW
+    0; JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,38 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+    // i=0
+    @i
+    M=0
+
+    // R[2] = 0
+    @R2
+    M=0
+
+// R[2] += R[0], R[1] times
+(LOOP)
+    // if i == R[1], goto END
+    @R1
+    D=M
+    @i
+    D=D-M
+    @END
+    D; JEQ
+
+    // R[2] += R[0]
+    @R0
+    D=M
+    @R2
+    M=M+D
+
+    // i=i+1
+    @i
+    M=M+1
+
+    @LOOP
+    0; JMP
+
+(END)
+    @END
+    0; JMP
